1f6d4eaa389ae3c570653e2cb34fab401a68f736
ElioFezollari <fezollarielio@gmail.com>
2024-06-21 16:37:54 -0400

    Fixed Images not displaying projerly

--- src/components/Index/Projects/Project.jsx
+++ src/components/Index/Projects/Project.jsx
@@ -1,56 +1,98 @@
-import './Project.css'
-import ProjectCard from "./ProjectCard/ProjectCard"
-import northPole from'../../../assets/Northpole.png' 
-import pathFusion from'../../../assets/PathFusion.png'
-import treasureMaze from'../../../assets/TreasureMaze.png'
-const Project = () =>{
-    const Projects = [
-        {
-            "Name" : "NorthPole Booking",
-            "img" : northPole,
-            "technology" : ["Asp.netCore","Javascript","html","Css","Bootstrap","Csharp","SqlServer","AJAX"],
-            "type" : "Fullstack",
-            "Description": "NorthPole Booking is a full stack asp.net core application for a booking website that includes bookings for flights, cars and hotels (which include rooms). This application utilizes LINQ, Entity Framework Core, Entity Framework Core identity and more! Its features include but not limited to the ability Sign Up/Logging in,sending emails (through the SendGrid API),Authorization and Authentication,Role Management,Reviews by regular users,Addition of new Items by admins,Booking    ",
-            "GitHub" : "https://github.com/ElioFezollari/NorthPole-Booking"
-        },
-        {
-            "Name" : "PathFusion",
-            "img" : pathFusion,
-            "technology" : ["PHP","Javascript","Html","Css","MYSQL","XAMPP","AJAX"],
-            "type" : "Fullstack",
-            "Description":  "PathFusion is a Learning Path creator and browser similar to LinkedIN learning, where users create, share and learn from each others learning paths. These learning paths are a detailed step by step guide on topics chosen by the user, and also categorized by them in different language, topics and more.The backend is completely built in PHP and the database is MYSQL.This app includes user login and registration,Upvote/Downvote System,User Comments,its own CMS and more all built from scratch using the mentioned technologies ",
-            "GitHub" : "https://github.com/ElioFezollari/PathFusion"
-        },
-        {
-            "Name" : "Treasure Maze",
-            "img" : treasureMaze,
-            "technology" : ["Java"],
-            "type" : "VideoGame",
-            "Description":  "Treasure Maze is a game built using only Java,JavaSwing and JavaFX. The purpose of the game is to collect the 7 treasures in the randomly-generated maze,and later escape through the door to advance to the next level.You have to do all of this without touching the purple walls which reset your position.The game also includes game saving and loading.",
-            "GitHub" : "https://github.com/ElioFezollari/Treasure-Maze"
-        }
-    ]
-    return(
-        <section className='project-sec-wrapper'>
-            <div className="project-wrapper">
-            <h2>Some of my work!</h2>
+import "./Project.css";
+import ProjectCard from "./ProjectCard/ProjectCard";
+import northPole from "../../../assets/Northpole.png";
+import pathFusion from "../../../assets/PathFusion.png";
+import treasureMaze from "../../../assets/TreasureMaze.png";
+import aspCore from "../../../assets/Asp.netCore.svg";
+import javascript from "../../../assets/javascript.svg";
+import html from "../../../assets/html.svg";
+import css from "../../../assets/css.svg";
+import bootstrap from "../../../assets/bootstrap.svg";
+import csharp from "../../../assets/csharp.svg";
+import sqlserver from "../../../assets/sqlserver.svg";
+import php from "../../../assets/php.svg";
+import mysql from "../../../assets/mysql.svg";
+import xampp from "../../../assets/ajax.svg";
+import ajax from "../../../assets/xampp.svg";
+import java from "../../../assets/java.svg";
+const Project = () => {
+  const images = [];
+  const Projects = [
+    {
+      Name: "NorthPole Booking",
+      img: northPole,
+      technology: {
+        name: [
+          "Asp.netCore",
+          "Javascript",
+          "html",
+          "Css",
+          "Bootstrap",
+          "Csharp",
+          "SqlServer",
+          "AJAX",
+        ],
+        techimg: [
+          aspCore,
+          javascript,
+          html,
+          css,
+          bootstrap,
+          csharp,
+          sqlserver,
+          ajax,
+        ],
+      },
 
-            <div className='projects'>
-            {Projects.map((project, index) => (
-                <ProjectCard 
-                    key={index}
-                    projectName={project.Name} 
-                    projectImg={project.img} 
-                    projectTechnologies={project.technology} 
-                    projectType={project.type} 
-                    projectDescription={project.Description} 
-                    projectGithub={project.GitHub} 
-                />
-            ))}
-                        </div>
-            </div>
-        </section>
-    )
-}
+      type: "Fullstack",
+      Description:
+        "NorthPole Booking is a full stack asp.net core application for a booking website that includes bookings for flights, cars and hotels (which include rooms). This application utilizes LINQ, Entity Framework Core, Entity Framework Core identity and more! Its features include but not limited to the ability Sign Up/Logging in,sending emails (through the SendGrid API),Authorization and Authentication,Role Management,Reviews by regular users,Addition of new Items by admins,Booking    ",
+      GitHub: "https://github.com/ElioFezollari/NorthPole-Booking",
+    },
+    {
+      Name: "PathFusion",
+      img: pathFusion,
+      technology: {
+        name: ["PHP", "Javascript", "Html", "Css", "MYSQL", "XAMPP", "AJAX"],
+        techimg: [php, javascript, html, css, mysql, xampp, ajax],
+      },
 
-export default Project
+      type: "Fullstack",
+      Description:
+        "PathFusion is a Learning Path creator and browser similar to LinkedIN learning, where users create, share and learn from each others learning paths. These learning paths are a detailed step by step guide on topics chosen by the user, and also categorized by them in different language, topics and more.The backend is completely built in PHP and the database is MYSQL.This app includes user login and registration,Upvote/Downvote System,User Comments,its own CMS and more all built from scratch using the mentioned technologies ",
+      GitHub: "https://github.com/ElioFezollari/PathFusion",
+    },
+    {
+      Name: "Treasure Maze",
+      img: treasureMaze,
+      technology: { name: ["Java"], techimg: [java] },
+      type: "VideoGame",
+      Description:
+        "Treasure Maze is a game built using only Java,JavaSwing and JavaFX. The purpose of the game is to collect the 7 treasures in the randomly-generated maze,and later escape through the door to advance to the next level.You have to do all of this without touching the purple walls which reset your position.The game also includes game saving and loading.",
+      GitHub: "https://github.com/ElioFezollari/Treasure-Maze",
+    },
+  ];
+  return (
+    <section className="project-sec-wrapper">
+      <div className="project-wrapper">
+        <h2>Some of my work!</h2>
+
+        <div className="projects">
+          {Projects.map((project, index) => (
+            <ProjectCard
+              key={index}
+              projectName={project.Name}
+              projectImg={project.img}
+              projectTechnologies={project.technology}
+              projectType={project.type}
+              projectDescription={project.Description}
+              projectGithub={project.GitHub}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Project;
--- src/components/Index/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Index/Projects/ProjectCard/ProjectCard.jsx
@@ -1,28 +1,47 @@
-import './ProjectCard.css';
-import github from "../../../../assets/github.svg"
-const ProjectCard = ({ projectName, projectImg, projectTechnologies, projectType, projectDescription, projectGithub }) => {
+import "./ProjectCard.css";
+import github from "../../../../assets/github.svg";
 
-    return (
-        <a className='project-link' href={projectGithub}>
-        <div className="project-card">
-            <div className='project-header-div'>
-            <h3>{projectName}</h3>
-            </div>
-            <img className="project-img" src={projectImg} alt="image of one of my projects" />
-            {/* Render project technologies */}
-            <div className='project-tech-container'>
-            {projectTechnologies.map((projectTech, index) => (
-                <img className='project-tech-img' src={`src/assets/${projectTech}.svg`} alt={projectTech} key={index} />
-            ))}
-            </div>
-            <hr />
-            <div className='project-type-div'><h4>Project Type: {projectType}</h4>            <a href={projectGithub}><img src={github} alt="Picture of github" /></a></div>
-            
-            <hr />
-            <p>{projectDescription}</p>
+const ProjectCard = ({
+  projectName,
+  projectImg,
+  projectTechnologies,
+  projectType,
+  projectDescription,
+  projectGithub,
+}) => {
+  return (
+    <a className="project-link" href={projectGithub}>
+      <div className="project-card">
+        <div className="project-header-div">
+          <h3>{projectName}</h3>
         </div>
-        </a>
-    );
-}
+        <img
+          className="project-img"
+          src={projectImg}
+          alt="image of one of my projects"
+        />
+        <div className="project-tech-container">
+          {projectTechnologies.techimg.map((projectTech, index) => (
+            <img
+              className="project-tech-img"
+              src={projectTech}
+              alt={projectTechnologies.name[index]}
+              key={index}
+            />
+          ))}
+        </div>
+        <hr />
+        <div className="project-type-div">
+          <h4>Project Type: {projectType}</h4>{" "}
+          <a href={projectGithub}>
+            <img src={github} alt="Picture of github" />
+          </a>
+        </div>
+        <hr />
+        <p>{projectDescription}</p>
+      </div>
+    </a>
+  );
+};
 
 export default ProjectCard;
